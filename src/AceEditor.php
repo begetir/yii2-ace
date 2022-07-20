e233ba0a36098fb8e8b5814c2caec8caa6abf14e
begetir <tameshki.ir@gmail.com>
2022-07-20 13:51:45 +0430

    updated

--- src/AceEditor.php
+++ src/AceEditor.php
@@ -1,159 +1,74 @@
 <?php
 namespace begetir\ace;
 
-use Yii;
+/
+ * Copyright  (C) 7/20/22, 3:32 AM , Inc - All Rights Reserved
+ * Author: Beget
+ * Programmer: Mohamad Kazem Lotfi
+ * Phone : +989130012987
+ * WebSite : https://beget.ir
+ * Powered By : Yii2  (https://www.yiiframework.com)
+ *
+ */
+
 use yii\helpers\Html;
 use yii\helpers\Json;
 use yii\widgets\InputWidget;
 
-/**
- * Class AceEditor
- * @package kak\widgets\aceeditor
+/
+ * ACE Widget
  */
 class AceEditor extends InputWidget
 {
+    /* @var boolean Read-only mode on/off (false = off - default) */
+    public $readOnly = false;
 
-    /**
-     * @var string Ace CDN base URL
-     */
-    public static $cdnBaseUrl = 'https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.1/';
+    / @var string By default, the editor supports plain text mode */
+    public $mode = 'php';
 
-    /** @var string */
-    public $varNameAceEditor = 'aceEditor';
-
-    /** @var bool Static syntax highlight */
-    public $editable = true;
-
-    /** @var bool */
-    public $autocompletion = false;
-
-    /**
-     * @var string Programming Language Mode
-     */
-    public $mode = 'html';
-
-    /**
-     * @var string Editor theme
-     * $see Themes List
-     * @link https://github.com/ajaxorg/ace/tree/master/lib/ace/theme
+    / @var string Themes are loaded on demand; all you have to do is pass the string name:
+     * See all themes https://github.com/ajaxorg/ace/tree/master/lib/ace/theme
      */
     public $theme = 'github';
 
-    /** @var array */
+    / @var bool Static syntax highlight */
+    public $editable = true;
+
+    / @var bool
+     * Auto completion snippets default is true
+     */
+    public $autocompletion = true;
+
+    / @var array */
     public $extensions = [];
 
-    /**
-     * @var array Ace options
-     * @see https://github.com/ajaxorg/ace/wiki/Configuring-Ace
-     */
+    / @var array */
     public $clientOptions = [
         "maxLines" => 100,
         "minLines" => 5,
     ];
 
-    /**
-     * @var array Container options
+    /
+     * @var array Div options
      */
     public $containerOptions = [
         'style' => 'width: 100%; min-height: 400px'
     ];
 
-    /**
-     * @inheritdoc
-     */
     public function init()
     {
         parent::init();
-        if (!isset($this->containerOptions['id'])) {
-            $this->containerOptions['id'] = $this->options['id'] . '-ace';
-        }
-        $this->initOptions();
-    }
+//        WidgetAsset::register($this->getView());
 
-    public function initOptions()
-    {
-        $id = $this->getId();
-
-
-        $editor_var = 'aceeditor_' . $id;
-        $textarea_var = 'acetextarea_' . $id;
-
-        $code = "
-            var {$editor_var} = ace.edit('{$id}')
-            {$editor_var}.setTheme('ace/theme/{$this->theme}')
-            {$editor_var}.getSession().setMode('ace/mode/{$this->mode}')
-            
-            var {$textarea_var} = $('#{$this->options['id']}').hide();
-            {$editor_var}.getSession().setValue({$textarea_var}.val());
-            {$editor_var}.getSession().on('change', function(){
-                {$textarea_var}.val({$editor_var}.getSession().getValue());
-            });
-        ";
-
-        $this->getView()->registerJs($code);
-
-        Html::addCssStyle($this->options, 'display: none');
-        $this->containerOptions['id'] = $id;
-        $this->getView()->registerCss("#{$id}{position:relative}");
 
     }
 
-    /**
-     * @inheritdoc
+    /
+     * @return string
      */
     public function run()
     {
         $this->registerPlugin();
-    }
-
-    /**
-     * Registers Ace plugin
-     */
-    protected function registerPlugin()
-    {
-        $view = $this->getView();
-        if (!$this->editable) {
-            $this->extensions[] = 'static_highlight';
-        }
-
-        if ($this->autocompletion) {
-            $this->extensions[] = 'language_tools';
-        }
-
-        WidgetAsset::register($view, $this->extensions);
-
-        return $this->editable ? $this->editable() : $this->readable();
-    }
-
-    protected function editable()
-    {
-        $id = $this->id;
-        $autocompletion = $this->autocompletion ? 'true' : 'false';
-
-        if ($this->autocompletion) {
-            $this->clientOptions['enableBasicAutocompletion'] = true;
-            $this->clientOptions['enableSnippets'] = true;
-            $this->clientOptions['enableLiveAutocompletion'] = false;
-        }
-
-        $clientOptions = Json::encode($this->clientOptions);
-        $js = <<<JS
-        (function(){
-            var aceEditorAutocompletion = {$autocompletion};
-            if (aceEditorAutocompletion) {
-                ace.require("ace/ext/language_tools");
-            }
-            {$this->varNameAceEditor} = ace.edit("{$id}");
-            {$this->varNameAceEditor}.setTheme("ace/theme/{$this->theme}");
-            {$this->varNameAceEditor}.getSession().setMode("ace/mode/{$this->mode}");
-            {$this->varNameAceEditor}.setOptions({$clientOptions});
-        })();
-JS;
-        $view = $this->getView();
-        $view->registerJs("\nvar {$this->varNameAceEditor} = {};\n", $view::POS_HEAD);
-        $view->registerJs($js);
-
-
         $content = Html::tag('div', '', $this->containerOptions);
         $this->options['style'] = 'display:none';
         if ($this->hasModel()) {
@@ -164,6 +79,66 @@ JS;
         return $content;
     }
 
+
+    protected function registerPlugin()
+    {
+        // Set ids
+        $editor_id = $this->getId();
+        $editor_var = 'aceeditor_' . $editor_id;
+        $textarea_var = 'acetextarea_' . $editor_id;
+
+        if (!$this->editable) {
+            $this->extensions[] = 'static_highlight';
+        }
+
+        // Enable auto completion
+        $autocompletion = $this->autocompletion ? 'true' : 'false';
+
+        if ($this->autocompletion) {
+            $this->extensions[] = 'language_tools';
+        }
+
+        if ($this->autocompletion) {
+            $this->clientOptions['enableBasicAutocompletion'] = true;
+            $this->clientOptions['enableSnippets'] = true;
+            $this->clientOptions['enableLiveAutocompletion'] = false;
+        }
+
+        WidgetAsset::register($this->getView(), $this->extensions);
+
+        // Convert client options to json
+        $clientOptions = Json::encode($this->clientOptions);
+
+        $js = <<<JS
+        (function(){
+            var aceEditorAutocompletion = {$autocompletion};
+            if (aceEditorAutocompletion) {
+                ace.require("ace/ext/language_tools");
+            }
+            {$editor_var} = ace.edit("{$editor_id}");
+            {$editor_var}.setTheme("ace/theme/{$this->theme}");
+            {$editor_var}.setHighlightActiveLine(true);
+            {$editor_var}.setShowPrintMargin(false);
+            {$editor_var}.getSession().setMode("ace/mode/{$this->mode}");
+            {$editor_var}.setReadOnly({$this->readOnly});
+            {$editor_var}.setOptions({$clientOptions});
+        })();
+JS;
+        $view = $this->getView();
+        $view->registerJs("\nvar {$editor_var} = {};\n", $view::POS_HEAD);
+        $view->registerJs($js);
+		$this->getView()->registerJs("
+            var {$textarea_var} = $('#{$this->options['id']}').hide();
+            {$editor_var}.getSession().setValue({$textarea_var}.val());
+            {$editor_var}.getSession().on('change', function(){
+                {$textarea_var}.val({$editor_var}.getSession().getValue());
+            });
+        ");
+        Html::addCssStyle($this->options, 'display: none');
+        $this->containerOptions['id'] = $editor_id;
+        $this->getView()->registerCss("#{$editor_id}{position:relative}");
+    }
+
     /**
      * @return string
      */
@@ -172,20 +147,19 @@ JS;
         $this->options['id'] = $this->id;
         $this->view->registerJs(
             <<<JS
-            (function(){
-                var _highlight = ace.require("ace/ext/static_highlight");
-                _highlight(\$('#{$this->id}')[0], {
-                    mode: "ace/mode/{$this->mode}",
-                    theme: "ace/theme/{$this->theme}",
-                    startLineNumber: 1,
-                    showGutter: true,
-                    trim: true
-                });
-            })();
+(function(){
+    var _highlight = ace.require("ace/ext/static_highlight");
+    _highlight(\$('#{$this->id}')[0], {
+        mode: "ace/mode/{$this->mode}",
+        theme: "ace/theme/{$this->theme}",
+        startLineNumber: 1,
+        showGutter: true,
+        trim: true
+    });
+})();
 JS
         );
 
         return Html::tag('pre', htmlspecialchars($this->value), $this->options);
     }
-
 }
